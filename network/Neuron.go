cba6f4320ccb52bc7878393561eeef32e9b3dcb7
Lennart Sandbothe <34800840+lennart1s@users.noreply.github.com>
2019-02-05 13:04:36 +0100

    Neuron erstmals kommentiert

--- network/Neuron.go
+++ network/Neuron.go
@@ -4,20 +4,58 @@ import (
 	UUID "github.com/google/uuid"
 )
 
+/* Neuron stellt ein einzelnes Neuron
+in einem Netzwerk dar. Die eingehenden
+Verbindungen mit anderen Neuronen wie
+Werte wie die letzte Eingabe und die
+letzte Ausgabe werden verwaltet.*/ 
 type Neuron struct {
+	/* Id ist die einamlige ID von 
+	einem Neuron.*/
 	Id UUID.UUID
 
+	/*Type beschreibt die Art des
+	Neurons und wie es folglich
+	Verwendet und berechnet wird.
+	Unterschieden wird zwischen den
+	vier Typen: INPUT, HIDDEN, BIAS
+	und OUTPUT.*/
 	Type int
 
+	/* Conns ist das Slice in dem die
+	eingehenden Verbindungen von anderen
+	Neuron gespeichert werden.*/
 	Conns []Connection
-
+	
+	/* Input speichert die letzte Gesamt-
+	eingabe. Diese ergibt sich aus der Summe
+	der Produkte aus Ausgabe und Gewichtung
+	aller eingehenden Werte.*/
 	Input  float64
+	/* Output speichert den letzten Ausgabewert.
+	Dieser brechnet sich aus der verwendeten
+	Aktivierungsfunktion in abhängigkeit von der
+	Gesamteingabe.*/
 	Output float64
 
+	/* Processed kann als Anhaltpunkt bei sämtlichen
+	Prozessen verwendet werden ob ein bestimmter
+	Schritt bereits abgeschlossen wurde.
+	Dieses Attribut wird weder vom Neuron selbst
+	noch vom Netzwerk verwendet.*/
 	Processed           bool
+	//TODO: ist das noch in gebrauch?
 	CalculatedGradients bool
 
+	/* Delta speichert den zuletzt berechneten
+	Deltawert. Dieser wird zum Trainieren eines
+	Netzwerkes mit Methoden, welche auf der 
+	Delta-Regel beruhen, verwendet.*/
 	Delta                  float64
+	/* ÜrevLayerWeightedDelta ist die Summe
+	der Produkte aus allen Delta-Werten von
+	Neuronen, die mit diesem Neuron verbunden
+	sind, und der Gewichtung dieser Verbindung.*/
 	PrevLayerWeightedDelta float64
 }
 
